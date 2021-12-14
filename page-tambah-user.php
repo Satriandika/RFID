5d48dbc8ed60216f3eb6cbd0b75319378aec5d71
Yudhya P W <yudhyapatriaw@outlook.com>
2021-12-15 03:41:52 +0700

    reformat

--- page-tambah-user.php
+++ page-tambah-user.php
@@ -1,94 +1,96 @@
-<?php 
+<?php
 
-   //Simpan dengan nama file panel.php
-     require "koneksidb.php";
-     $data    = query("SELECT * FROM tb_monitoring")[0];
-     
+//Simpan dengan nama file panel.php
+require "koneksidb.php";
+$data    = query("SELECT * FROM tb_monitoring")[0];
 
- ?>
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <!-- test commit -->
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Top Up</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="assets/images/delameta.ico" />
-  </head>
-  <body>
-  <?php 
-        session_start();
-        if($_SESSION['status']!="login"){
-            header("location:index.php?pesan=belum_login");
-        }
+<!-- test commit -->
 
-        if(isset($_GET['pesan'])){
-            if($_GET['pesan'] == "berhasil"){
-                 echo "<script type='text/javascript'>alert('Berhasil menambahkan data!');</script>";
-            }
-            if($_GET['pesan'] == "gagal"){
-                echo "<script type='text/javascript'>alert('Data Tidak Terdaftar!');</script>";
-            }
-         }
-	?>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Top Up</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="assets/images/delameta.ico" />
+</head>
 
-    <div class="container-scroller">
-      <!-- partial:partials/_navbar.html -->
+<body>
+  <?php
+  session_start();
+  if ($_SESSION['status'] != "login") {
+    header("location:index.php?pesan=belum_login");
+  }
+
+  if (isset($_GET['pesan'])) {
+    if ($_GET['pesan'] == "berhasil") {
+      echo "<script type='text/javascript'>alert('Berhasil menambahkan data!');</script>";
+    }
+    if ($_GET['pesan'] == "gagal") {
+      echo "<script type='text/javascript'>alert('Data Tidak Terdaftar!');</script>";
+    }
+  }
+  ?>
+
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <?php
+    require("page-header.php");
+    ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
       <?php
-          require("page-header.php");
+      require("page-sidebar.php");
       ?>
+
       <!-- partial -->
-      <div class="container-fluid page-body-wrapper">
-        <!-- partial:partials/_sidebar.html -->
-        <?php
-                require("page-sidebar.php");
-            ?>
-            
-        <!-- partial -->
-        <div class="main-panel">
-          <div class="content-wrapper">
-            <div class="page-header">
-              <h3 class="page-title">
-                <span class="page-title-icon bg-gradient-primary text-white mr-2">
-                  <i class="mdi mdi-currency-usd"></i>
-                </span> Menu Top Up
-              </h3>
-            </div>
-            <div class="row">
-						<div class="col-md-12 grid-margin stretch-card">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Top Up</h4>
-                    <p class="card-description">Masukkan RFID dan Nominal</p>
-                    <form action="action-tambah-user.php" method="POST" name="form-tambah-user" class="forms-sample">
-                      <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control" name="username" placeholder="Masukan username">
-                      </div>
-                      <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" placeholder="Masukan password">
-                      </div>
-                      <button type="submit" name="submit" value="Submit" class="btn btn-gradient-primary mr-2">Submit</button>
-                      <button onclick="cancelSubmit()" class="btn btn-light">Cancel</button>
-                    </form>
-                  </div>
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="page-header">
+            <h3 class="page-title">
+              <span class="page-title-icon bg-gradient-primary text-white mr-2">
+                <i class="mdi mdi-currency-usd"></i>
+              </span> Menu Top Up
+            </h3>
+          </div>
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Top Up</h4>
+                  <p class="card-description">Masukkan RFID dan Nominal</p>
+                  <form action="action-tambah-user.php" method="POST" name="form-tambah-user" class="forms-sample">
+                    <div class="form-group">
+                      <label for="username">Username</label>
+                      <input type="text" class="form-control" name="username" placeholder="Masukan username">
+                    </div>
+                    <div class="form-group">
+                      <label for="password">Password</label>
+                      <input type="password" class="form-control" name="password" placeholder="Masukan password">
+                    </div>
+                    <button type="submit" name="submit" value="Submit" class="btn btn-gradient-primary mr-2">Submit</button>
+                    <button onclick="cancelSubmit()" class="btn btn-light">Cancel</button>
+                  </form>
                 </div>
               </div>
             </div>
+          </div>
           <!-- content-wrapper ends -->
           <!-- partial:partials/_footer.html -->
           <footer class="footer">
