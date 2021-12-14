ebc839e812cdbf81354afecf8a80ce0bc273cc1c
yudhya p w <yudhya.wicaksono@delameta.com>
2021-12-14 16:03:57 +0700

    add dataapi, page log data, error map, remove files

---
+++ const-error-map.php
@@ -0,0 +1,7 @@
+<?php
+    define("BERHASIL", "Berhasil");
+    define("GAGAL", "Gagal");
+    define("RFID_NOT_FOUND", "RFID tidak dikenal");
+    define("TOL_NOT_FOUND", "Tol tidak dikenal");
+    define("SALDO_KURANG", "Saldo kurang");
+?>
