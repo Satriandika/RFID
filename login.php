880e86165fc1f37e7f8d5b484e7c8a06db074dac
yudhya p w <yudhya.wicaksono@delameta.com>
2021-12-14 13:56:06 +0700

    pull master

---
+++ login.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Login</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="assets/images/delameta.ico" />
+</head>
+
+<body>
+  <?php
+  if (isset($_GET['pesan'])) {
+    if ($_GET['pesan'] == "gagal") {
+      echo "<script type='text/javascript'>alert('Login gagal! username dan password salah!');</script>";
+    } else if ($_GET['pesan'] == "logout") {
+      echo "<script type='text/javascript'>alert('Anda telah berhasil logout!');</script>";
+    } else if ($_GET['pesan'] == "belum_login") {
+      echo "<script type='text/javascript'>alert('Anda harus login terlebih dahulu!');</script>";
+    }
+  }
+  ?>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth">
+        <div class="row flex-grow">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left p-5">
+              <div class="brand-logo">
+                <center><img src="assets/images/delameta2.svg"></center>
+              </div>
+              <center>
+                <h4>SELAMAT DATANG</h4>
+              </center>
+              <center>
+                <h6 class="font-weight-light">Silahkan Login</h6>
+              </center>
+              <form class="pt-3" action="action-login.php" method="post" onSubmit="return validasi()">
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="mt-3">
+                  <input type="submit" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" name="login" value="LOGIN">
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input">Biarkan saya tetap masuk</label>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="assets/js/off-canvas.js"></script>
+  <script src="assets/js/hoverable-collapse.js"></script>
+  <script src="assets/js/misc.js"></script>
+  <script type="text/javascript">
+            function validasi() {
+                var username = document.getElementById("username").value;
+                var password = document.getElementById("password").value;		
+                if (username != "" && password!="") {
+                    return true;
+                }else{
+                    alert('Username dan Password harus di isi !');
+                    return false;
+                }
+            }
+        
+        </script>
+  <!-- endinject -->
+</body>
+
+</html>
